345b77428b0a1cd4c8deef019d5bbbe5a83fc562
Trev <trevor.pawlewicz@gmail.com>
2016-11-10 12:11:32 -0500

    added bootstrap to header.php

--- includes/partials/header.php
+++ includes/partials/header.php
@@ -15,6 +15,9 @@
 <head>
     <meta charset="utf-8">
     <title>phillyChat!</title>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 
 <body>
