c0d16378b829a0b1c9e97d3fc454126cb55de468
Trev <trevor.pawlewicz@gmail.com>
2016-11-10 11:30:29 -0500

    initial commit

---
+++ includes/form_handlers/login_handler.php
@@ -0,0 +1,33 @@
+<?php
+
+    if (isset($_POST['login_button'])) {
+
+        $email = filter_var($_POST['log_email'], FILTER_VALIDATE_EMAIL);
+
+        $_SESSION['log_email'] = $email; // store email in SESSION variables
+        $password = md5($_POST['log_password']);
+
+        $check_database_query = mysqli_query($connect, "SELECT * FROM users WHERE email='$email' AND password='$password'");
+        $check_login_query = mysqli_num_rows($check_database_query);
+
+        if ($check_login_query == 1) {
+            $row = mysql_fetch_array($check_database_query);
+            $username = $row['username'];
+
+            //                                                 ALL
+            $user_closed_query = mysqli_query($connect, "SELECT * FROM users WHERE email='$email' AND user_closed='yes'");
+
+            if (mysqli_num_rows($user_closed_query) == 1) {
+                $reopen_account = mysqli_query($connect, "UPDATE users SET user_closed='no' WHERE email='email'");
+            }
+
+            $_SESSION['username'] = $username;
+            header("Location: index.php");
+            exit();
+
+        } else {
+            array_push($error_array, "Email or Password was Incorrect! <br>");
+        }
+    }
+
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    require 'config/config.php';
+?>
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>phillyChat!</title>
+</head>
+
+<body>
+    <h1>Trev's PHP!</h1>
+</body>
+</html>
