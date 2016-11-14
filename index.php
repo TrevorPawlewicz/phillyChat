b4c75f1b7f6e62a3696177b482849673422ba5d5
Trev <trevor.pawlewicz@gmail.com>
2016-11-14 15:40:18 -0500

    styling index.php - user box name, likes, posts

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <?php
     include('includes/header.php');
-    // session_destroy();
 ?>
 
         <div class="user-details column">
+
             <a href="#">
                 <img src="<?php echo $user['profile_pic']; ?>"/>
             </a>
+
+            <div class="user-details-left-right">
+                <a href="#">
+                    <?php
+                        echo $user['first_name'] . " " . $user['last_name'];
+
+                    ?>
+                </a>
+
+                <br>
+
+                <?php
+                    echo "Posts: " . $user['num_posts']. "<br>";
+                    echo "Linkes: " . $user['num_likes'];
+                ?>
+            </div>
+
         </div>
 
 
