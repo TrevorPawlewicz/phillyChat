e3aabb1835018a7fc023167025ded4323a12859d
Trev <trevor.pawlewicz@gmail.com>
2016-11-15 16:40:55 -0500

    updating Post class

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -40,5 +40,26 @@
                 $update_query = mysqli_query($this -> connect, "UPDATE users SET num_posts='$num_posts' WHERE username='$added_by'");
             }
         }
+
+        public function loadPostsFriends() {
+            $str = "";
+            $data = mysqli_query($this -> connect, "SELECT * FROM posts WHERE deleted='no' ORDER BY id DESC");
+
+            while ($row = mysqli_fetch_array($data)) {
+                $id        = $row['id'];
+                $body      = $row['body'];
+                $added_by  = $row['added_by'];
+                $date_time = $row['date_added'];
+
+                // prepare user_to string so it can be included even if not posted to a user:
+                if ($row['user_to'] == 'none') {
+                    $user_to = "";
+                } else {
+                    $user_to_object = new User($connect, $row['user_to']);
+                    $user_to_name = $user_to_object -> getFirstAndLastName();
+                    $user_to = "<a href='" . $row['user_to'] ."'>" . $user_to_name . "</a>";
+                }
+            }
+        }
     }
 ?>
